af7c85be9ab1263a32bf772955b494c0f8e87717
Manan Agarwal <62429009+MananAg007@users.noreply.github.com>
2022-01-31 18:28:32 +0530

    Added Header

--- client_/src/App.jsx
+++ client_/src/App.jsx
@@ -10,6 +10,24 @@ import PointsTablePage from './routes/PointsTablePage';
 const App = () => {
     return (
   <ContextProvider>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Panda Cricket</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="http://localhost:3000/matches">Matches</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost:3000/venues">Venues</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
        <div className='container'>
            <Router>
                 <Switch>
