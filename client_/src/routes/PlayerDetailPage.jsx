4186802f877e2f6be413c56197ecd004ea4ee7c7
Manan Agarwal <mananagarwal007@gmail.com>
2022-02-02 02:09:03 +0530

    minor edits

--- client_/src/routes/PlayerDetailPage.jsx
+++ client_/src/routes/PlayerDetailPage.jsx
@@ -8,7 +8,16 @@ import {Bar, Doughnut} from 'react-chartjs-2';
 const PlayerDetailPage = () => {
   const { id } = useParams();
   const {V1,SV1, V2, SV2, V3, SV3, V4, SV4, V5, SV5, V6, SV6, V7, SV7, V8, SV8} = useContext(Context)
-
+  const divStyleLeft = {
+    width: '49.5%', 
+    float: 'left',
+    'text-align': 'center'
+  };
+  const divStyleRight = {
+    width: '49.5%', 
+    float: 'right',
+    'text-align': 'center'
+  };
   useEffect( ()=> {
     const fetchData = async () => {
         try {
@@ -28,7 +37,7 @@ const PlayerDetailPage = () => {
     fetchData();
 },[]) 
 
-return (<div className='list-group'>
+return (<div >
 {/* <canvas id="lineChart" height="400" width="400"></canvas> */}
   <h1 className='font-weight-light display-1 text-center'>Player Details </h1>
 <table className="table table-hover table-dark">
@@ -45,6 +54,8 @@ return (<div className='list-group'>
         <tr><td>Bowling Skill</td><td>{V1.bowling_skill}</td></tr>
     </tbody>
 </table>
+
+<div style = {divStyleLeft}>
 <h1 className='font-weight-light display-1 text-center'>Batting Statistics </h1>
 <table className="table table-hover table-dark">
     <thead>
@@ -64,35 +75,8 @@ return (<div className='list-group'>
         <tr><td>Average</td><td>{V4.average}</td></tr>
     </tbody>
 </table>
-<Bar
-          data={{
-            labels: V7.map((crypto) => crypto.match_id),
-            datasets: [
-              {
-                label: 'Runs scored',
-                backgroundColor: 'rgba(75,192,192,1)',
-                borderColor: 'rgba(0,0,0,1)',
-                borderWidth: 2,
-                data: V7.map((cr) => cr.sum)
-              }
-            ]
-          }}
-          options={{
-            plugins:{
-              title:{
-                display:true,
-                text:'Runs scored',
-                fontSize:20
-              },
-              legend:{
-                display:true,
-                position:'right'
-              }
-            }
-            
-          }}
-        />
-<h1 className='font-weight-light display-1 text-center'>Bowling Statistics </h1>
+</div>
+<div style ={divStyleRight}><h1 className='font-weight-light display-1 text-center'>Bowling Statistics </h1>
 <table className="table table-hover table-dark">
     <thead>
       <tr className='bg-primary'>
@@ -108,9 +92,39 @@ return (<div className='list-group'>
         <tr><td>Wickets</td><td>{V5.outs}</td></tr>
         <tr><td>Economy</td><td>{V5.avg}</td></tr>
         <tr><td>Five Wickets</td><td>{V6.count}</td></tr>
+        <tr><td> -</td><td>-</td></tr>
     </tbody>
-</table>
-<Bar
+</table></div>
+<div style = {divStyleLeft}><Bar
+          data={{
+            labels: V7.map((crypto) => crypto.match_id),
+            datasets: [
+              {
+                label: 'Runs scored',
+                backgroundColor: 'rgba(75,192,192,1)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderWidth: 2,
+                data: V7.map((cr) => cr.sum)
+              }
+            ]
+          }}
+          options={{
+            plugins:{
+              title:{
+                display:true,
+                text:'Runs scored',
+                fontSize:20
+              },
+              legend:{
+                display:true,
+                position:'top'
+              }
+            }
+            
+          }}
+        /></div>
+
+<div style={divStyleRight}><Bar
           data={{
             labels: V8.map((crypto) => crypto.match_id),
             datasets: [
@@ -126,7 +140,7 @@ return (<div className='list-group'>
                   type: 'bar',
                 label: 'Runs Conceded',
                 backgroundColor: 'rgba(75,192,192,1)',
-                borderColor: 'rgba(0,0,0,1)',
+                borderColor: 'rgba(75,192,192,1)',
                 borderWidth: 2,
                 data: V8.map((cr) => cr.sum)
               },
@@ -142,12 +156,13 @@ return (<div className='list-group'>
               },
               legend:{
                 display:true,
-                position:'right'
+                position:'top'
               }
             }
             
           }}
-        />
+        /></div>
+
 </div>
 
 );
