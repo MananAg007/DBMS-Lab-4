11a068f4e975b87f80948711d15549d79dd0e452
Manan Agarwal <mananagarwal007@gmail.com>
2022-02-05 16:59:34 +0530

    removed libraries

--- client_/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return <div>
-      <h1 className='font-weight-light display-1 text-center'>Venue Table</h1>
-  </div>;
-};
-
-export default Header;
